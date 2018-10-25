d934230c0e62931672cbeb388abda113bb2f8696
Carl <cjones6343@gmail.com>
2018-10-25 13:46:42 -0400

    Added a new sysout

--- src/MainApp.java
+++ src/MainApp.java
@@ -4,6 +4,7 @@ public class MainApp {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Goodbye World!");
+		System.out.println("You could have been better!!!");
 
 	}
 
