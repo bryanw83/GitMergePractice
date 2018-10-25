6f88b4a95e7689e01644622254d4327c21766426
bryan <bryanw83@yahoo.com>
2018-10-25 13:33:13 -0400

    Added a question

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,6 +2,9 @@
 public class MainApp {
 
 	public static void main(String[] args) {
+		
+		
+		System.out.println("Shall we play a game?");
 		System.out.println("Hello World!");
 		System.out.println("Goodbye World!");
 
