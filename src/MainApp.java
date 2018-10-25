12654681f2bd0553f484e9c809965306a497ff46
bryan <bryanw83@yahoo.com>
2018-10-25 13:43:07 -0400

    Fixing stuff

--- src/MainApp.java
+++ src/MainApp.java
@@ -6,7 +6,7 @@ public class MainApp {
 		
 		System.out.println("Shall we play a game?");
 		System.out.println("Hello World!");
-		System.out.println("Goodbye World!");
+		System.out.println("Goodbye, you...you...nasty 'ol World!");
 
 	}
 
