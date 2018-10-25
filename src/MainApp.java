7112b36defd5c847b3aacd01b7700dbcc6dcadd9
Carl <cjones6343@gmail.com>
2018-10-25 13:56:20 -0400

    Changes to goodbye line

--- src/MainApp.java
+++ src/MainApp.java
@@ -6,7 +6,7 @@ public class MainApp {
 		
 		System.out.println("Shall we play a game?");
 		System.out.println("Hello World!");
-		System.out.println("Goodbye World!");
+		System.out.println("Goodbye World! How could thee have forsaken me!");
 		System.out.println("You could have been better!!!");
 
 	}
